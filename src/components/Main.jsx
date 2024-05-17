0d052b0cb1b74e1a51efef2e67ad150323a558a3
shalri <fsaonlinework777@gmail.com>
2024-05-18 00:11:46 +0800

    Desktop Layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,25 +2,28 @@ import FeatureCard from "./FeatureCard";
 
 const Main = () => {
   return (
-    <main className="container mx-auto mb-auto flex flex-1 flex-col items-center justify-center">
-      <article className="px-8 pb-7 pt-8">
+    // <main className="container mx-auto mb-auto flex flex-1 flex-col items-center justify-center">
+    <main className="mx-auto mb-auto flex flex-1 flex-col items-center justify-center sm:max-w-full">
+      <article className="px-8 pb-7 pt-8 sm:px-0">
         <h1>
-          <span className="mb-1 block text-center text-2xl font-normal text-ffs-very-dark-blue">
+          <span className="mb-1 block text-center text-2xl font-normal text-ffs-very-dark-blue sm:mb-3 sm:text-4xl">
             Reliable, efficient delivery
           </span>
-          <span className=" block text-center text-2xl font-bold text-ffs-very-dark-blue">
+          <span className=" block text-center text-2xl font-bold text-ffs-very-dark-blue sm:text-4xl">
             Powered by Technology
           </span>
         </h1>
-        <p className="my-4 text-center leading-relaxed text-ffs-grayish-blue">
+        <p className="my-4 text-center leading-relaxed text-ffs-grayish-blue sm:max-w-[508px] sm:text-[1rem]">
           Our Arificial Intelligence powered tools use millions of project data
           point to ensure that your project is successful
         </p>
       </article>
-      <section className="grid gap-6 p-8">
+      <section className="flex flex-col gap-6 px-8 py-8 sm:grid  sm:max-w-[1112px] sm:grid-cols-3 sm:items-center sm:gap-8 sm:px-0">
         <FeatureCard featureName="Supervisor" />
-        <FeatureCard featureName="Team Builder" />
-        <FeatureCard featureName="Karma" />
+        <div className="flex flex-col gap-6 sm:gap-8">
+          <FeatureCard featureName="Team Builder" />
+          <FeatureCard featureName="Karma" />
+        </div>
         <FeatureCard featureName="Calculator" />
       </section>
     </main>
